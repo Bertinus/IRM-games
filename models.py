809a23b43ebed054d97ae229ccebe6e0f342f64e
clementjumel <clement.jumel@polytechnique.edu>
2020-05-01 10:58:36 -0400

    Add models & optimizers modules

---
+++ models.py
@@ -0,0 +1,53 @@
+from torch import nn
+from tensorflow import keras
+
+
+class TorchModel(nn.Module):
+    def __init__(self, length, width, height, n_classes):
+        super().__init__()
+
+        self.layer1 = nn.Linear(length * width * height, 390)
+        self.layer2 = nn.Linear(390, 390)
+        self.layer3 = nn.Linear(390, n_classes)
+        self.dropout = nn.Dropout(p=0.75)
+        self.activation = nn.ELU()
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        x = x.view(batch_size, -1)  # Flatten the input
+
+        x = self.dropout(x)
+        x = self.layer1(x)
+        x = self.activation(x)
+
+        x = self.dropout(x)
+        x = self.layer2(x)
+        x = self.activation(x)
+
+        x = self.layer3(x)
+
+        return x
+
+
+def keras_model(length, width, height, n_classes):
+    return keras.Sequential([keras.layers.Flatten(input_shape=(length, width, height)),
+                             keras.layers.Dense(390,
+                                                activation='elu',
+                                                kernel_regularizer=keras.regularizers.l2(0.00125)),
+                             keras.layers.Dropout(0.75),
+                             keras.layers.Dense(390,
+                                                activation='elu',
+                                                kernel_regularizer=keras.regularizers.l2(0.00125)),
+                             keras.layers.Dropout(0.75),
+                             keras.layers.Dense(n_classes)])
+
+
+def create_models(module_name, n_env, length, width, height, n_classes):
+    if module_name == 'keras':
+        return [keras_model(length=length, width=width, height=height, n_classes=n_classes) for _ in range(n_env)]
+
+    elif module_name == 'pytorch':
+        return [TorchModel(length=length, width=width, height=height, n_classes=n_classes) for _ in range(n_env)]
+
+    else:
+        raise Exception('Module must be "keras" or "pytorch".')
